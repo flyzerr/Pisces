3610b9d156d0e9476a206f7c6a847751e7c914a8
flyzer <38090128+flyzerr@users.noreply.github.com>
2023-03-20 22:20:23 +0800

    Update __init__.py

--- plato/datasources/yolov5/__init__.py
+++ plato/datasources/yolov5/__init__.py
@@ -1,1 +1,8 @@
+import os
+import sys
+from pathlib import Path
 
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
