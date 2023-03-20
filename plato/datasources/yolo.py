0f2c5c5ad55e0dabd412c0ddbb103dc78d3c0846
flyzer <38090128+flyzerr@users.noreply.github.com>
2023-03-20 18:55:09 +0800

    Update yolo.py

--- plato/datasources/yolo.py
+++ plato/datasources/yolo.py
@@ -20,8 +20,8 @@ import torch
 from plato.config import Config
 from plato.datasources.yolov5 import utils
 # from yolov5.utils.datasets import LoadImagesAndLabels
-from yolov5.utils.dataloaders import LoadImagesAndLabels
-from yolov5.utils.general import check_img_size
+from plato.datasources.yolov5.utils.dataloaders import LoadImagesAndLabels
+from plato.datasources.yolov5.utils.general import check_img_size
 
 from plato.datasources import base
 
