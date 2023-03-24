bc6fe3d61a3a29ff50c838e93154dc7d8a28e924
flyzerr <1938384271@qq.com>
2023-03-25 02:02:20 +0800

    *

--- plato/datasources/base.py
+++ plato/datasources/base.py
@@ -132,9 +132,16 @@ class DataSource:
             # print(self.trainset.labels) # 有类别有位置 [array([[0, 0.45156, 0.675, 0.37813, 0.35833]], dtype=float32), array([[0, 0.45312, 0.69583, 0.38125, 0.35]], dtype=float32) ....
             # return self.trainset.labels
 
-            print(type(self.trainset.labels[:, 0]))
-            print(self.trainset.labels[:, 0])
-            return self.trainset.labels[:, 0]
+            # print(type(self.trainset.labels[:, 0]))
+            # print(self.trainset.labels[:, 0])
+            # return self.trainset.labels[:, 0]
+        
+            ret = []
+            for i in self.trainset.labels:
+                ret.append(int(i[0][0]))
+            print(type(ret))
+            print(ret)
+            return ret
         ###################################################################
         return self.trainset.targets
 
